3615ce8926aa8c944d4501a6ce55da09d0b14501
shannagcps <62246778+shannagcps@users.noreply.github.com>
2020-04-08 19:44:58 -0400

    Removed empty lists

--- SGP_CONFIG/2018_2019/MASTER_CONFIG.R
+++ SGP_CONFIG/2018_2019/MASTER_CONFIG.R
@@ -1,91 +1,106 @@
 ################################################################################
 ###                                                                          ###
-###      SGP Configurations code for 2019 Elem/Middle School MATH/Alg 1      ###
+###      SGP Configurations code for 2019 ALL TESTS                          ###
 ###                                                                          ###
 ################################################################################
 
-#source("Gwinnett_Course_Progressions_2018_2019.R")
+source("Gwinnett_Course_Progressions_2018_2019.R")
 
 ### could maybe just select the top one in each progression to do
 # would include rules here about which ones should be run
 # sample size restrictions; number of progressions etc.
+
 df_to_run <- df2 %>% 
   arrange(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0, desc(pct)) %>% 
   filter(!is.na(immediate_prior)) %>% 
-  group_by(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0) %>% 
+  group_by(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0, YEAR) %>% 
   mutate(s = as.integer(row_number() == 1L)) %>% 
   ungroup() %>% 
   filter(s == 1) %>% 
-  filter(ct>=1200) %>% 
-# take out precalculus to see if will run - SINGULAR MATRIX ERROR
-# filter(CONTENT != "PRECALCULUS") %>% 
-#   filter(CONTENT != "SPANISH_III")
-  # THESE ARE THE ONES THAT ARE WORKING
-  # NEED TO TEST TO SEE WHY IT BREAKS AND THROWS AN ERROR ABOUT KNOT BOUNDARIES
-  filter(CONTENT %in% c("WORLD_HISTORY",
-                        "WORLD_GEOGRAPHY",
-                        "US_HISTORY"))
+  filter(ct>=1200)
 
-tests <- df_to_run %>% 
+
+tests_spring <- df_to_run %>% 
+  filter(YEAR == "2018_2019.2") %>% 
+  ungroup() %>% 
+  distinct(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0) %>% 
+  filter(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0 != "BIOLOGY.EOCT")%>% 
+  # BIOLOGY WAS BREAKING IT THEN IT BROKE AGAIN
+  # Singular design matrix error
+  pull(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0) 
+
+tests_fall <- df_to_run %>% 
+  filter(YEAR == "2018_2019.1") %>% 
   ungroup() %>% 
   distinct(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0) %>% 
   pull(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0)
 
-years <- df_to_run %>% 
-  ungroup() %>% 
-  distinct(YEAR) %>% 
-  pull(YEAR)
-
 # create empty list
 config_list <- list()
+config_list_spring <- list()
+config_list_fall <- list()
 
-# Spring to Spring
-for (i in tests){
-  a.1 <-  df_to_run %>% 
-      filter(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0 == i) %>% 
-      filter(YEAR == "2018_2019.1")
-  content.1 <- a.1$CONTENT
-  content_prior.1 <- a.1$content_prior
-  year.1 <- a.1$YEAR
-  year_prior.1 <- a.1$immediate_prior_year
-  grade.1 <- a.1$GRADE
-  grade_prior.1 <- a.1$grade_prior
-  #s.1 <- a_1$s
-  
+# Spring 2018-2019 Tests
+for (i in tests_spring){
   a.2 <-  df_to_run %>% 
     filter(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0 == i) %>% 
-    filter(YEAR == "2018_2019.2")
-  content.2 <- a.2$CONTENT
-  content_prior.2 <- a.2$content_prior
+    filter(YEAR == "2018_2019.2") 
+  
+    content.2 <- a.2$CONTENT
+    content_prior.2 <- a.2$content_prior
   year.2 <- a.2$YEAR
   year_prior.2 <- a.2$immediate_prior_year
   grade.2 <- a.2$GRADE
   grade_prior.2 <- a.2$grade_prior
   #s.2 <- a_2$s
   
-      i.j.config <- list(
-        i.j.config.1 <- list(
-        sgp.content.areas = c(content_prior.1, content.1),
-        sgp.panel.years = c(year_prior.1, year.1),
-        sgp.grade.sequences = list(c(grade_prior.1, grade.1)
-                 )
-        ,
-        sgp.norm.group.preference = 2),
-        
-        i.j.config.2 <- list(
-          sgp.content.areas = c(content_prior.2, content.2),
-          sgp.panel.years = c(year_prior.2, year.2),
-          sgp.grade.sequences = list(
-            c(grade_prior.2, grade.2)
-                     )
-          ,
-          sgp.norm.group.preference = 1)
-        
+  i.j.config <- list(
+      i.j.config.2 <- list(
+      sgp.content.areas = c(content_prior.2, content.2),
+      sgp.panel.years = c(year_prior.2, year.2),
+      sgp.grade.sequences = list(
+        c(grade_prior.2, grade.2)
       )
-      
-     config_list <- append(config_list, i.j.config)
+      ,
+      sgp.norm.group.preference = 1)
+    
+  )
+  
+  config_list <- append(config_list, i.j.config)
+}
+config_list_fall <- config_list
+
+config_list <- list()
+# Fall 2018-2019 Tests
+for (i in tests_fall){
+a.1 <-  df_to_run %>%
+  filter(CONTENT_AREA_by_GRADE_PRIOR_YEAR.0 == i) %>% 
+  filter(YEAR == "2018_2019.1") 
+  content.1 <- a.1$CONTENT
+content_prior.1 <- a.1$content_prior
+year.1 <- a.1$YEAR
+year_prior.1 <- a.1$immediate_prior_year
+grade.1 <- a.1$GRADE
+grade_prior.1 <- a.1$grade_prior
+#s.1 <- a_1$s
+
+i.j.config <- list(
+  i.j.config.1 <- list(
+    sgp.content.areas = c(content_prior.1, content.1),
+    sgp.panel.years = c(year_prior.1, year.1),
+    sgp.grade.sequences = list(c(grade_prior.1, grade.1)
+    )
+    ,
+    sgp.norm.group.preference = 2)
+)
+config_list <- append(config_list, i.j.config)
 }
 
+config_list_spring <- config_list
+config_list <- list()
+config_list <- append(config_list_fall,
+                      config_list_spring)
+  
 # older versions
 # MATHEMATICS_2018_2019.2.config <- list(
 # MATHEMATICS.2018_2019.1 <- list(
