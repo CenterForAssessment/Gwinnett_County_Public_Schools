e2a147de7bed18ad5ea33955f5845ca21235e7e0
shannagcps <62246778+shannagcps@users.noreply.github.com>
2020-04-08 14:38:23 -0400

    Add files via upload
    sources MASTER_CONFIG.R file

--- Gwinnett_Simulated_SGP_2018_2019.R
+++ Gwinnett_Simulated_SGP_2018_2019.R
@@ -10,32 +10,48 @@ require(data.table)
 
 
 ###  Load cleaned long data (from Gwinnett_Simulated_Data_LONG.R)
-load("Data/Simulated_Data/Gwinnett_Data_LONG-Resimulated.Rdata") # load("../4_output/datasets/Gwinnett_Data_LONG.Rdata")
-load("Data/Simulated_Data/Gwinnett_Data_INSTRUCTOR_NUMBER_Weighted.Rdata")
-
+#load("Data/Simulated_Data/Gwinnett_Data_LONG.Rdata")
+load("../4_output/datasets/Gwinnett_Data_LONG.Rdata")
 
 ###  Read in 2019 SGP Configuration Scripts (from Github Repo)
 
-source("SGP_CONFIG/2018_2019/MATHEMATICS.R")
-source("SGP_CONFIG/2018_2019/LANGUAGE_ARTS.R")
+#source("SGP_CONFIG/2018_2019/MATHEMATICS.R")
+#source("SGP_CONFIG/2018_2019/LANGUAGE_ARTS.R")
+source("SGP_CONFIG/2018_2019/MASTER_CONFIG.R")
 
 ###  Combine 2019 SGP Configuration Scripts
+# GCPS.config <- c(
+#   LANGUAGE_ARTS_2018_2019.2.config,
+#   MATHEMATICS_2018_2019.2.config,
+#   ALGEBRA_I_2018_2019.2.config)
 GCPS.config <- c(
-  LANGUAGE_ARTS_2018_2019.2.config,
-  MATHEMATICS_2018_2019.2.config,
-  ALGEBRA_I_2018_2019.2.config)
+  config_list
+)
 
 
 #####
-###   Modify SGPstateData for use with higher level functions (analyzeSGP)
+###   Add minimal SGPstateData for use with higher level functions (analyzeSGP)
 #####
 
-###   Still need to add in the "GCPS" knots and boundaries
-###   All other basic meta-data has been added to SGPstateData (SGP version 1.9-5.1)
+###   Add in the "GCPS" elements needed
+###   Eventually this will be added to SGPstateData and included in the SGP package
+###   For now we will just manually change/add to the SGPstateData object
 
-###   Create knots and boundaries from data (actual or simulated)
-gcps_knots_boundaries <- createKnotsBoundaries(Gwinnett_Data_LONG)
-SGPstateData[["GCPS"]][["Achievement"]][["Knots_Boundaries"]] <- gcps_knots_boundaries
+###   Create knots and boundaries from simulated data
+gcps_sim_kbs <- createKnotsBoundaries(Gwinnett_Data_LONG)
+# save(gcps_sim_kbs, file = "./Data/Simulated_Data/gcps_sim_kbs.rda") # Save if desired
+
+SGPstateData[["GCPS"]][["Achievement"]][["Knots_Boundaries"]] <- gcps_sim_kbs
+
+SGPstateData[["GCPS"]][["Assessment_Program_Information"]][["CSEM"]] <- "SCALE_SCORE_CSEM"
+SGPstateData[["GCPS"]][["Assessment_Program_Information"]] <- NULL
+
+SGPstateData[["GCPS"]][["Growth"]][["Levels"]] <- c("Low", "Typical", "High")
+SGPstateData[["GCPS"]][["Growth"]][["System_Type"]] <- "Cohort Referenced"
+SGPstateData[["GCPS"]][["Growth"]][["Cutscores"]] <-
+	list(
+		Cuts=c(35, 66),
+		Labels=list("1st - 34th", "35th - 65th", "66th - 99th"))
 
 
 #####
@@ -44,22 +60,9 @@ SGPstateData[["GCPS"]][["Achievement"]][["Knots_Boundaries"]] <- gcps_knots_boun
 
 ###   Step 1.  prepareSGP (Create a SGP object with Long Data)
 
-Gwinnett_SGP <- prepareSGP(
-  Gwinnett_Data_LONG,
-  data_supplementary=list(INSTRUCTOR_NUMBER=Gwinnett_Data_INSTRUCTOR_NUMBER),
-  create.additional.variables=FALSE)
-
-#  The LONG data is located in the @Data slot.
-names(Gwinnett_Data_LONG)
-names(Gwinnett_SGP@Data)
-
-#  The teacher - student link table is located in the @Data_Supplementary slot.
-names(Gwinnett_SGP@Data_Supplementary[["INSTRUCTOR_NUMBER"]])
-
+Gwinnett_SGP <- prepareSGP(Gwinnett_Data_LONG, state = 'GCPS', create.additional.variables=FALSE)
 
 ###   Step 2.  analyzeSGP (produce SGPs for progressions specified in config scripts)
-
- # Added SIMEX calculations - assumes SCALE_SCORE_CSEM Has been calculated from RELIABILITY
 
 Gwinnett_SGP <- analyzeSGP(
   Gwinnett_SGP,
@@ -71,19 +74,18 @@ Gwinnett_SGP <- analyzeSGP(
   sgp.percentiles.baseline = FALSE,
   sgp.projections.baseline = FALSE,
   sgp.projections.lagged.baseline = FALSE,
-  simulate.sgps=TRUE,
-  calculate.simex = TRUE) # Added SIMEX argument
+  simulate.sgps=FALSE)
+
 
 ###   Step 3.  combineSGP (merge raw results into the long data (@Data slot))
 
-Gwinnett_SGP <- combineSGP(Gwinnett_SGP)
+Gwinnett_SGP <- combineSGP(Gwinnett_SGP, 
+                           state = 'GCPS')
 
 
 ###   Step 4.   outputSGP (Write long data with results to working directory)
 
-outputSGP(Gwinnett_SGP,
-          outputSGP.directory="Data/Simulated_Data", # Change to desired output location
-          output.type = c("LONG_Data", "LONG_FINAL_YEAR_Data")) # WIDE still not working with GRADE duplicates
+outputSGP(Gwinnett_SGP,  state = 'GCPS', output.type = c("LONG_Data", "LONG_FINAL_YEAR_Data")) # WIDE still not working with GRADE duplicates
 
 ###   Quick summary of results
 
@@ -91,20 +93,4 @@ outputSGP(Gwinnett_SGP,
 Gwinnett_SGP@Data[!is.na(SGP) & VALID_CASE=='VALID_CASE'][, list(
   Test_Scores = round(cor(SCALE_SCORE, SCALE_SCORE_PRIOR_STANDARDIZED, use='pairwise.complete'), 2),
   SGP = format(round(cor(SGP, SCALE_SCORE_PRIOR_STANDARDIZED, use='pairwise.complete'), 2), nsmall = 2),
-  SGP_SIMEX_RANKED = format(round(cor(SGP_SIMEX_RANKED, SCALE_SCORE_PRIOR_STANDARDIZED, use='pairwise.complete'), 2), nsmall = 2),
   N_Size = sum(!is.na(SGP))), keyby = list(CONTENT_AREA, GRADE)]
-
-
-###   Step 5.  summarizeSGP (merge raw results into the long data (@Data slot))
-
-Gwinnett_SGP <- summarizeSGP(Gwinnett_SGP)
-
-#  Summary tables are located in the @Summary slot.
-names(Gwinnett_SGP@Summary)
-names(Gwinnett_SGP@Summary[["STATE"]])
-
-#  Access the Teacher summary table disaggregated by YEAR, CONTENT_AREA and GRADE
-Gwinnett_SGP@Summary[["STATE"]][["STATE__INSTRUCTOR_NUMBER__CONTENT_AREA__YEAR__GRADE"]]
-
-###   Save Gwinnett SGP Object
-save(Gwinnett_SGP, file="Data/Simulated_Data/Gwinnett_SGP.Rdata")
